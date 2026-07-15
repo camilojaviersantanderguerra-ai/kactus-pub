fb09e29ebeb6c66c8e29fea78fd61ffeb9e0bb35
camilojaviersantanderguerra-ai <camilojaviersantanderguerra@gmail.com>
2026-07-14 23:17:38 -0400

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import { Hero } from "@/components/sections/Hero";
+import { Eventos } from "@/components/sections/Eventos";
+import { PorQueElegir } from "@/components/sections/PorQueElegir";
+import { Nosotros } from "@/components/sections/Nosotros";
+import { Galeria } from "@/components/sections/Galeria";
+import { ArriendoLocal } from "@/components/sections/ArriendoLocal";
+import { Testimonios } from "@/components/sections/Testimonios";
+import { FAQ } from "@/components/sections/FAQ";
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <div className="divider-gradient" />
+      <Eventos />
+      <PorQueElegir />
+      <Nosotros />
+      <Galeria />
+      <ArriendoLocal />
+      <Testimonios />
+      <FAQ />
+
+      {/*
+        En la próxima etapa se agrega acá: <Contacto />
+      */}
+    </>
+  );
+}
